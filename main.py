c73d3bc9f8766fb7414bf1fb54fc52dc103e4c8e
Hunter W <kfateweaver@gmail.com>
2024-01-09 14:16:37 -0500

    Update main.py

--- main.py
+++ main.py
@@ -82,17 +82,6 @@ class light:
         return "RGB:<"+str(self.red)+", "+str(self.green)+", "+str(self.blue)+">"
 
 
-active_viewers = 0
-stream_lock = threading.Lock()
-def video_stream_monitor():
-    global active_viewers ,stream_lock
-    while True:
-        with stream_lock:
-            if active_viewers == 0:
-                PyCamLightControls.dbg_msg("No viewers detected. Stopping encoding.")
-                PyCamLightControls.stop_camera_stream()
-                return
-        time.sleep(10)  # Check every 10 seconds
 
 class PyCamLightControls:
     GPIO_RED = 17
@@ -265,6 +254,18 @@ class PyCamLightControls:
         sc.capture_file(data, format='jpeg')
         return data.getvalue()
 
+
+active_viewers = 0
+stream_lock = threading.Lock()
+def video_stream_monitor():
+    global active_viewers ,stream_lock
+    while True:
+        with stream_lock:
+            if active_viewers == 0:
+                PyCamLightControls.dbg_msg("No viewers detected. Stopping encoding.")
+                PyCamLightControls.stop_camera_stream()
+                return
+        time.sleep(10)  # Check every 10 seconds
 
 
 # Routes
